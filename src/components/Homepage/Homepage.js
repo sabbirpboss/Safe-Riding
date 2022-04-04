8f40fe0a86fd50d9c2209e738a773203783d4161
sabbirpboss <sabbir.ah799@gmail.com>
2022-04-05 00:52:45 +0600

    homepage component done

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import banner from '../../images/banner.png';
 import './Homepage.css';
 
 const Homepage = () => {
     return (
-        <div>
-            <h2>This is homepage</h2>
+        <div className='homepage'>
+            <div className='banner-details'>
+                <h1 className='banner-title'>Royal enfield meteor</h1>
+                <h1 className='custom-color'>The king of the roads</h1>
+                <p>The Royal Enfield Meteor 350 cruiser motorcycle was unveiled by Royal Enfield as a replacement for the Thunderbird 350 worldwide.</p>
+                <button className='banner-btn'>See More</button>
+            </div>
+            <div className='banner-img'>
+                <img src={banner} alt="" />
+            </div>
         </div>
     );
 };
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>This page is not available</h2>
+        </div>
+    );
+};
+
+export default NotFound;
