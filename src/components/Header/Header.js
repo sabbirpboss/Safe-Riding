42235a04367dc6e5bdbbbb39b888ef0e8fee24c3
sabbirpboss <sabbir.ah799@gmail.com>
2022-04-04 15:23:05 +0600

    created fakedata

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <a href="">
+                <img src="" alt="" />
+            </a>
+        </nav>
+    );
+};
+
+export default Header;
